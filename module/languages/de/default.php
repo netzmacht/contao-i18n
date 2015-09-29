1d6c2a967383aa3dc9e635b173a202a18678782a
David Molineus <david.molineus@netzmacht.de>
2015-09-29 09:26:41 +0200

    Add translation.

---
+++ module/languages/de/default.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Contao I18n provides some i18n structures for easily l10n websites.
+ *
+ * @package    dev
+ * @author     David Molineus <david.molineus@netzmacht.de>
+ * @copyright  2015 netzmacht creative David Molineus
+ * @license    LGPL 3.0
+ * @filesource
+ *
+ */
+
+$GLOBALS['TL_LANG']['PTY']['i18n_regular'][0] = 'I18n Reguläre Seite';
+$GLOBALS['TL_LANG']['PTY']['i18n_regular'][0] = 'Eine reguläre I18n Seite lädt den Inhalt der verknüpften Sprachfallback-Seite.';
+
+$GLOBALS['TL_LANG']['CTE']['i18n'][0] = 'I18n';
+$GLOBALS['TL_LANG']['CTE']['i18n'][1] = 'Internationalization';
---
+++ module/languages/de/modules.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Contao I18n provides some i18n structures for easily l10n websites.
+ *
+ * @package    dev
+ * @author     David Molineus <david.molineus@netzmacht.de>
+ * @copyright  2015 netzmacht creative David Molineus
+ * @license    LGPL 3.0
+ * @filesource
+ *
+ */
+
+$GLOBALS['TL_LANG']['MOD']['i18n'][0] = 'I18n';
+$GLOBALS['TL_LANG']['MOD']['i18n'][1] = 'Internationalization';
+
+$GLOBALS['TL_LANG']['FMD']['i18n'][0] = 'I18n';
+$GLOBALS['TL_LANG']['FMD']['i18n'][1] = 'Internationalization';
+
+$GLOBALS['TL_LANG']['FMD']['i18n_navigation'] = 'I18n Navigationmenü';
+$GLOBALS['TL_LANG']['FMD']['i18n_navigation'] = 'Erzeugt ein Navigationsmenü aus der Seitenstruktur mit Berücksichtigung der I18n Einstellungen';
+
+$GLOBALS['TL_LANG']['FMD']['i18n_navigation'] = 'I18n Navigationmenü';
+$GLOBALS['TL_LANG']['FMD']['i18n_navigation'] = 'Erzeugt ein Navigationsmenü aus der Seitenstruktur mit Berücksichtigung der I18n Einstellungen';
