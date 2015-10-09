61d6dce3b0676ad34efec94741f52480f04c1123
David Molineus <david.molineus@netzmacht.de>
2015-10-09 22:07:46 +0200

    English translation.

---
+++ module/languages/en/modules.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Contao I18n provides some i18n structures for easily l10n websites.
+ *
+ * @package    dev
+ * @author     David Molineus <david.molineus@netzmacht.de>
+ * @copyright  2015 netzmacht creative David Molineus
+ * @license    LGPL 3.0
+ * @filesource
+ *
+ */
+
+$GLOBALS['TL_LANG']['MOD']['i18n'][0] = 'I18n';
+$GLOBALS['TL_LANG']['MOD']['i18n'][1] = 'Internationalization';
+
+$GLOBALS['TL_LANG']['FMD']['i18n'][0] = 'I18n';
+$GLOBALS['TL_LANG']['FMD']['i18n'][1] = 'Internationalization';
+
+$GLOBALS['TL_LANG']['FMD']['i18n_navigation'][0] = 'I18n navigation menu';
+$GLOBALS['TL_LANG']['FMD']['i18n_navigation'][1] = 'I18n navigation menu uses the translated reference page.';
+
+$GLOBALS['TL_LANG']['FMD']['i18n_form'][0] = 'I18n form';
+$GLOBALS['TL_LANG']['FMD']['i18n_form'][1] = 'I18n redirects to the translated page.';
